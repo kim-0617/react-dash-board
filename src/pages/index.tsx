9ef1b735b633fe6e1fc6133176a8e873b468fe87
kim-0617 <kkk5993@naver.com>
2023-07-10 20:52:48 +0900

    ECommerce.tsx 파일 겹치는거 다시 삭제하고 재시도

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 export { default as Calendar } from "./Calendar";
 export { default as ColorPicker } from "./ColorPicker";
 export { default as Customers } from "./Customers";
-export { default as ECommerce } from "./Ecommerce";
+export { default as ECommerce } from "./ECommerce";
 export { default as Editor } from "./Editor";
 export { default as Employees } from "./Employees";
 export { default as Kanban } from "./Kanban";
