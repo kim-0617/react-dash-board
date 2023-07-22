fb42725012dedf635177ed0ac9945a204ce349f7
kim-0617 <kkk5993@naver.com>
2023-07-23 01:37:29 +0900

    Employees 섹션 완성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = ({ category, title }: IHeader) => {
   return (
     <div className="mb-10">
       <p className="text-gray-400">{category}</p>
-      <p className="text-3xl fontextrabold tracking-tight text-slate-900">
+      <p className="text-3xl font-extrabold tracking-tight text-slate-900">
         {title}
       </p>
     </div>
--- src/pages/Employees.tsx
+++ src/pages/Employees.tsx
@@ -1,7 +1,38 @@
 import React from "react";
+import {
+  GridComponent,
+  ColumnsDirective,
+  Page,
+  Search,
+  Inject,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
+import { employeesData, employeesGrid } from "../data/dummy";
+import { Header } from "../components";
+import { ColumnDirective } from "@syncfusion/ej2-react-charts";
 
 const Employees = () => {
-  return <div>Employees</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Employees" category="Page" />
+
+      <GridComponent
+        id="gridcomp"
+        dataSource={employeesData}
+        allowPaging
+        toolbar={["Search"]}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Employees;
